83f2e8154ab7f9ce39978cef2b5be8da13f76c2c
Ana Carolina <45365825+printf-ana@users.noreply.github.com>
2020-06-03 19:08:15 -0300

    Add files via upload
    Atualização de código feita no dia 03/06/2020. Adição de mais alguns códigos JS e CSS.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -13,19 +13,24 @@ function populateUFs () {
 populateUFs()
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateSelect = document.querySelector("select[name=state]")
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
     const  ufValue = event.target.value
 
-    const indexOfSelectdState = event.target.selectedIndex
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
-    .then(res => res.json() ) /* também poderia ser (res) => { return res.json() }) */
+    .then(res => res.json() ) 
     .then(cities => {
         for( const city of cities) {
-            citySelect.innerHTML +=  `<option value="${city.id}">${city.nome}</option>` /*crase serve p interpolar*/
+            citySelect.innerHTML +=  `<option value="${city.nome}">${city.nome}</option>` 
         }
         citySelect.disabled = false        
     } )
@@ -34,3 +39,49 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+
+
+//Itens de coleta
+//pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect) {
+    item.addEventListener("click",handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items")
+
+let selectedItems = [] /*let p poder atualizar, const n pode sobrescrever */
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    //verificar se existem itens selecionados, se sim, quais
+    
+    const alreadySelected = selectedItems.findIndex( item =>  {
+        const itemFound = item == itemId  // será true or false
+        return itemFound
+    })
+
+
+    //se ja estiver selecionado, tirar da seleção
+    if (alreadySelected >= 0) {
+        //tirar da seleção
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    } 
+    else { //caso não, adicionar a seleção
+        selectedItems.push(itemId)
+    }
+    //atualizar o campo escondido com os dados selecionados
+    collectedItems.value = selectedItems
+}
+
